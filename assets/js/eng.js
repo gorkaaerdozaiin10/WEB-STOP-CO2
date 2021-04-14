9eea8ca946d205305798324b437da4f51404445c
gorkaaerdozaiin10 <gorkaaerdozaiin10@gmail.com>
2021-04-14 19:54:23 +0200

    Co-authored-by: marioiba9 <marioiba9@users.noreply.github.com>

---
+++ assets/js/eng.js
@@ -0,0 +1,69 @@
+document.getElementById('pop-up-1-eng').onclick = function () {
+  Swal.fire({
+    title: 'Preparation',
+    html:
+      '<section class= "popupsweet"' +
+      '<ul>' +
+      '<li>Cuatrovientos sets out in its Strategic Plan 18-21 the action: 1.1.5 Analyze IC CSR' +
+      ' Cuatrovientos <br></li>' +
+      '<li>Diagnosis using the INNOVARSE methodology of the Government of Navarra (2019), which' +
+      ' concludes: <i> The Environmental Dimension is the dimension that offers the greatest scope for improvement ' +
+      '(oportunities)</i></li>' +
+      '<li>The Internationalization team requests and grants to lead an Erasmus project on' +
+      ' The Carbon Footprint, in order to develop the environmental dimension of the Centre.</li>' +
+      '<li>Management appoints a team responsible for the dynamisation of the project, composed of ' +
+      'the team of axes: <b>internationalisation, commitment and innovation + an external environmental consultant.</b>' +
+      '</li>' +
+      '<li>The situation of the Centre is analyzed and the CHALLENGE of the Project is defined: <b>“BEING NEUTRAL CENTRE ON EMISIONS"</b></li>' +
+      '<li> Gymkana Awareness-raising Organization: 21 September to 16 October (4 weeks).</li>' +
+      '<li>Analyze the situation of the Centre, which has the participation of students: ' +
+      'measure the carbon footprint of the Centre and ' +
+      'propose possible actions to reduce and offset emissions</li>' +
+      '<li>As a result of this event and with the collaboration of the coordinating team, Management and our' +
+      ' environmental advisor establish:' +
+      '<ul>' +
+      '<li>Cuatroviento´s Environmental Statement</li>' +
+      '<li>A report on the carbon footprint and its MEC certification.</li>' +
+      '<li>' +
+      '<a href="http://www.cuatrovientos.org/proyecto-stop-co2-memoria/">A plan to reduce and offset emissions </a></li>' +
+      '</ul>' +
+      '</li>' +
+      '</ul>' +
+      '</section>',
+    width: '75rem',
+    confirmButtonColor: '#01d091',
+  });
+};
+
+document.getElementById('pop-up-2-eng').onclick = function () {
+  Swal.fire({
+    title: 'Development',
+    html:
+      '<section class= "popupsweet"' +
+      '<ul>' +
+      '<li>The management of the Centre and the coordinating team implement different measures:' +
+      ' 100% renewable energy certification, installation of a renewable energy production system in the building and planting of 100 trees.' +
+      '</li>' +
+      '<li> Activities aimed at meeting the challenge are carried out during the Cultural Week of the Centre (Art Recyclers, ODS Training)</li>' +
+      '<li>The environmental variable is introduced into the 2nd CCSS project module.</li>' +
+      '<li>Students in the first year of each cycle will work in the classroom (working day) to develop a product that will' +
+      ' to develop a product that will contribute to the measures proposed in <b> The reduction and ' +
+      ' Compesation plan</b>.</li>' +
+      '</ul>' +
+      '</section>',
+    width: '75rem',
+    confirmButtonColor: '#01d091',
+  });
+};
+
+document.getElementById('pop-up-3-eng').onclick = function () {
+  Swal.fire({
+    title: 'Closure',
+    html:
+      '<section class= "popupsweet"' +
+      '<img src="800px-Proyecto_en_construccion.jpg">' +
+      '</section>',
+    width: '75rem',
+    confirmButtonColor: '#01d091',
+  });
+};
